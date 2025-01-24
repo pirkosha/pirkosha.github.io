f2ab0c7561ed445a3a8573b3bb6f2000e2c17942
pirkosha <pirkosha@gmail.com>
2025-01-25 01:18:04 +0300

    Create CQG21.php

---
+++ CQG21.php
@@ -0,0 +1,197 @@
+<?php include "../Header.php"; ?>
+<html>
+
+<head>
+
+   <title>C*-algebras and compact quantum groups. Spring 2021</title>
+
+   <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=KOI8-R">
+
+</head>
+
+<body bgcolor="#F5DEB3">
+
+<!-- begin of Top100 code -->
+
+<a href="http://top100.rambler.ru/top100/">
+
+<img src="http://counter.rambler.ru/top100.cnt?188597" alt="" width=1
+height=1 border=0></a>
+
+<!--end of Top100 code-->
+
+<!--
+<p align="right">
+<a href="../index.html">На главную</a>
+<br>
+<a href="../schedule.html">Расписание занятий</a>
+</p>    -->
+
+			      
+<h2><a HREF="#top"></a>C*-algebras and compact quantum groups. Spring 2021</h2>
+<h3> <a href="http://www.hse.ru/en/staff/pirkovskii">A. Yu. Pirkovskii</a> </h3>
+<!--<p>Модули:&nbsp;
+<a HREF="#m1">I</a>,&nbsp;
+<a HREF="#m2">II</a>&nbsp;
+<a HREF="#m3">III</a>,&nbsp;
+<a HREF="#m4">IV</a>.&nbsp;&nbsp;
+<a HREF="#m5">V</a>.&nbsp;&nbsp;
+<a HREF="#exams">Зачёты и экзамены</a>&nbsp;&nbsp;
+
+<h3><A NAME="m1">Модуль I</A></h3>
+
+<p>        -->
+<!-- <a HREF="#m1books">Учебники</a>&nbsp;&nbsp;
+<a HREF="#m1program">Программа</a>&nbsp;&nbsp;
+<a HREF="#m1lectures">Учебные материалы к лекциям</a>&nbsp;&nbsp;
+<a HREF="#m1problems">Задачи семинаров</a>&nbsp;&nbsp;
+<a HREF="#m1test">Контрольные материалы</a>&nbsp;&nbsp; -->
+
+<p>
+<a href="../pspdf/2021/cqg/CQG21_en.pdf"><b>Syllabus</b></a>
+
+  <dl>
+        <dt> <b>Lecture 1</b> (14.01.2021) <b>(<a href="https://youtu.be/B_HtPQrKBaU">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_01.pdf">lecture notes</a>)</b></dt>
+<dd>Historical remarks on C*-algebras and compact quantum groups.
+Banach algebras. Basic examples of Banach algebras (function algebras, operator algebras, group L<sup>1</sup>-algebras of
+discrete groups). A survey of spectral theory in Banach algebras (the spectrum
+of an algebra element, algebraic properties of the spectrum, properties of the multiplicative group of a Banach
+algebra, the continuity of characters, the compactness and the nonemptiness of the spectrum,
+the Gelfand-Mazur theorem).</dd>
+	<dt> <b>Lecture 2</b> (21.01.2021) <b>(<a href="https://youtu.be/mxzQ_byW0WA">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_02.pdf">lecture notes</a>)</b></dt>
+<dd>The spectral radius formula. The maximal spectrum and the character space of a commutative unital algebra.
+The closedness of maximal ideals of a commutative unital Banach algebra.
+The 1-1 correspondence between the character space and the maximal spectrum of a commutative unital Banach algebra.
+Some properties of the weak* topology.
+The Gelfand topology on the maximal spectrum. The compactness of the maximal spectrum (the unital case).
+The Gelfand transform of a commutative unital Banach algebra. Remarks on the Jacobson radical.</dd>
+	<dt> <b>Lecture 3</b> (28.01.2021) <b>(<a href="https://youtu.be/1MzXqjxtpEE">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_03.pdf">lecture notes</a>)</b></dt>
+<dd>The maximal spectrum and the Gelfand transform for subalgebras of C(X). Functorial properties of the Gelfand transform
+(the adjoint functors X &rarr; C(X) and A &rarr; Max(A)). Unitization. The unitization of C<sub>0</sub>(X) and the one-point
+compactification of X. The spectrum of an element of a nonunital algebra. Modular ideals.
+The maximal spectrum and the Gelfand transform for nonunital Banach algebras.</dd>
+	<dt> <b>Lecture 4</b> (04.02.2021) <b>(<a href="https://youtu.be/gxltfJZ1LnA">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_04.pdf">lecture notes</a>)</b></dt>
+<dd>Banach *-algebras. C*-algebras. Examples. Products and unitizations of C*-algebras.
+The spectral radius of a normal element is equal to the norm. The automatic continuity of *-homomorphisms to C*-algebras.
+The spectrum of a selfadjoint element is real.</dd>
+        <dt> <b>Lecture 5</b> (11.02.2021) <b>(<a href="https://youtu.be/okfaXmGaHr0">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_05.pdf">lecture notes</a>)</b></dt>
+<dd>Characters of C*-algebras preserve involution. The spectral invariance of C*-subalgebras.
+The 1st (commutative) Gelfand-Naimark theorem. A category-theoretic interpretation of the Gelfand-Naimark theorem.
+Injective *-homomorphisms between C*-algebras are isometric. The continuous functional calculus in C*-algebras.
+The spectral mapping theorem and the superposition property for the functional calculus.</dd>
+	<dt> <b>Lecture 6</b> (18.02.2021) <b>(<a href="https://youtu.be/x23lNL8UswY">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_06.pdf">lecture notes</a>)</b></dt>
+<dd>Positive elements in C*-algebras. Properties of positive elements.
+Square roots. Kaplansky's theorem (x*x&ge;0). Characterizations of positivity. The order structure on a C*-algebra.
+Approximate identities in Banach algebras. Examples.</dd>
+	<dt> <b>Lecture 7</b> (25.02.2021) <b>(<a href="https://youtu.be/CjmW5LRhqd4">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_07.pdf">lecture notes</a>)</b></dt>
+<dd>The existence of approximate identities in C*-algebras. Quotient C*-algebras.
+Positive functionals on a C*-algebra. Examples and basic properties of positive functionals.</dd>
+        <dt> <b>Lecture 8</b> (04.03.2021) <b>(<a href="https://youtu.be/1sBcA9HvwLk">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_08.pdf">lecture notes</a>)</b></dt>
+<dd>Positivity criteria for linear functionals on a C*-algebra.
+Extension and existence of positive functionals. *-representations and *-modules.  The GNS construction.
+An abstract characterization of GNS representations.</dd>
+	<dt> <b>Lecture 9</b> (11.03.2021) <b>(<a href="https://youtu.be/2rD5sCacxK0">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_09.pdf">lecture notes</a>)</b></dt>
+<dd>Hilbert direct sums of *-representations.
+The universal representation of a C*-algebra. The 2nd Gelfand-Naimark theorem (the existence of
+an isometric *-representation of a C*-algebra). Nondegenerate and cyclic *-representations.
+Decomposing nondegenerate representations into cyclic summands.</dd>
+	<dt> <b>Lecture 10</b> (18.03.2021) <b>(<a href="https://youtu.be/9oAYwc7TmSo">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_10.pdf">lecture notes</a>)</b></dt>
+<dd>GNS representations and cyclic vectors. Complex conjugate spaces. Tensor products of algebras and of *-algebras.
+The C*-algebra structure on the matrix algebra M<sub>n</sub>(A) (existence and uniqueness).</dd>
+	<dt> <b>Lecture 11</b> (25.03.2021) <b>(<a href="https://youtu.be/DrGhy8dyQ3k">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_11.pdf">lecture notes</a>)</b></dt>
+<dd>Tensor products of Hilbert spaces, of operators on Hilbert spaces, and of *-representations.
+The spatial C*-norm and the spatial tensor product of C*-algebras. Takesaki's theorem on the minimality
+of the spatial norm (without proof). The functoriality of the spatial C*-tensor product.</dd>
+	<dt> <b>Lecture 12</b> (01.04.2021) <b>(<a href="https://youtu.be/2MnZZoqlAu8">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_12.pdf">lecture notes</a>)</b></dt>
+<dd>The independence of the spatial C*-norm on the choice of faithful representations. Examples:
+the spatial tensor product of K(H) by K(H); the spatial tensor product by C<sub>0</sub>(X).
+The maximal C*-tensor product.</dd>
+	<dt> <b>Lecture 13</b> (08.04.2021) <b>(<a href="https://youtu.be/_ARNR7iuPdo">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_13.pdf">lecture notes</a>)</b></dt>
+<dd>Properties of the maximal C*-tensor product. Nuclear C*-algebras. Examples. Remarks on tensor products
+of locally convex spaces and on nuclear spaces. C*-envelopes and universal C*-algebras.
+Sufficient conditions for the existence of C*-envelopes.</dd>
+	<dt> <b>Lecture 14</b> (15.04.2021) <b>(<a href="https://youtu.be/H_FEmMdgLro">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_14.pdf">lecture notes</a>)</b></dt>
+<dd>Examples of C*-envelopes. Coalgebras, bialgebras, unital C*-bialgebras. A characterization
+of compact groups as compact semigroups with cancellation. Compact quantum groups.
+The Gelfand-Naimark-Woronowicz duality.</dd>
+	<dt> <b>Lecture 15</b> (22.04.2021) <b>(<a href="https://youtu.be/XMDXr0xDdhI">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_15.pdf">lecture notes</a>)</b></dt>
+<dd>The universal property of C(SU(2)). The quantum SU(2) group.
+Compact matrix quantum groups. The quantum SU(2) as the C*-envelope of the quantum SL(2).</dd>
+	<dt> <b>Lecture 16</b> (29.04.2021) <b>(<a href="https://youtu.be/DTHjLFkqGVQ">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_16.pdf">lecture notes</a>)</b></dt>
+<dd>Slice maps on spatial C*-tensor products. Left invariant and right invariant
+functionals on compact quantum groups. The convolution product
+on the dual space of a compact quantum group. Invariant functionals in terms of convolutions.
+The existence and the uniqueness of a Haar state on a compact quantum group.</dd>
+	<dt> <b>Lecture 17</b> (13.05.2021) <b>(<a href="https://youtu.be/Apg0c14zWwk">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_17.pdf">lecture notes</a>)</b></dt>
+<dd>The existence of the Haar state (end of proof). Finite-dimensional corepresentations of compact quantum groups.
+Intertwining maps. An equivalence between representations of G and corepresentations of C(G).
+Matrix corepresentations. The fundamental corepresentation of a compact matrix quantum group.</dd>
+	<dt> <b>Lecture 18</b> (20.05.2021) <b>(<a href="https://youtu.be/lRy4-nwftvU">video</a>)</b>
+<b>(<a href="../pspdf/2021/cqg/CQG_lec_18.pdf">lecture notes</a>)</b></dt>
+<dd>The embedding of the category of corepresentations of a compact quantum group A into the category of representations
+of A*. Invariant subspaces for corepresentations; the role of the dual algebra. Irreducble corepresentations.
+Schur's lemma. Finite-dimensional corepresentations of compact quantum groups are unitarizable. A corollary:
+compact matrix quantum groups are unitarizable. A characterization of commutative compact matrix quantum groups.
+Philosophy: compact matrix quantum groups = compact quantum Lie groups. Concluding remarks
+(decomposing into irreducibles, the dense Hopf *-subalgebra of matrix elements, Tannaka-Krein duality).</dd>
+</dl>
+
+
+<h3><A NAME="m1problems">Exercise sheets</A></h3>
+
+
+<ul>
+<li><a href="../pspdf/2021/cqg/CQG_list_01.pdf">Exercise sheet 1</a>. Commutative Banach algebras
+<li><a href="../pspdf/2021/cqg/CQG_list_02.pdf">Exercise sheet 2</a>. C*-algebras. Functional calculus. Positivity
+<li><a href="../pspdf/2021/cqg/CQG_list_03.pdf">Exercise sheet 3</a>. Approximate identities. Positive functionals. GNS construction
+<li><a href="../pspdf/2021/cqg/CQG_list_04.pdf">Exercise sheet 4</a>. Tensor products
+<li><a href="../pspdf/2021/cqg/CQG_list_05.pdf">Exercise sheet 5</a>. C*-envelopes
+<li><a href="../pspdf/2021/cqg/CQG_list_06.pdf">Exercise sheet 6</a>. Compact quantum groups
+<li><a href="../pspdf/2021/cqg/CQG_list_07.pdf">Exercise sheet 7</a>. Free compact quantum groups
+</ul>
+
+<!-- <ul>
+<li><a href="../pspdf/1415/cqg/CQG_exam.pdf">Take-home exam</a>
+</ul>  -->          
+
+<a href="../pspdf/2021/cqg/biblio.pdf"><h3>References</h3></a>
+
+<p>
+
+<!-- <a href="../pspdf/1314/harmonic/harmonic_exer.pdf">Список задач (under construction)</a> -->
+
+<!-- <a href="../pspdf/1415/meas_int/meas_exam.pdf">Экзамен</a>. Срок сдачи - 26 декабря.    -->
+
+<p>
+<!-- <a HREF="#top">Top</a>&nbsp;&nbsp;          -->
+<!-- <a href="../schedule.html">Расписание занятий</a>&nbsp;&nbsp;  -->
+
+<!-- 
+<a HREF="#exams">Зачёт и экзамен</a>&nbsp;&nbsp;
+-->
+
+
+<!-- Для более темного оттенка будем использовать #D2B48C -->
+
+
+
+</body>
+
+</html>
